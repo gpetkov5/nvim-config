7a6e75ac02eebdda1f9b0108880c01ce16b2f47b
gpetkov5 <georgi.petkov.ox@gmail.com>
2025-01-30 08:10:28 +0000

    Fix breaking changes with blink

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,58 +1,61 @@
 return {
-  "saghen/blink.cmp",
-  version = "*",
-  dependencies = "rafamadriz/friendly-snippets",
+	"saghen/blink.cmp",
+	version = "*",
+	dependencies = "rafamadriz/friendly-snippets",
 
-  opts = {
-    keymap = {
-      preset = "none",
-      ["<C-space>"] = { "fallback" },
-      ["<C-h>"] = { "show_documentation", "hide_documentation" },
-      ["<C-e>"] = { "hide", "fallback" },
-      ["<CR>"] = { "accept", "fallback" },
+	opts = {
+		keymap = {
+			preset = "none",
+			["<C-space>"] = { "fallback" },
+			["<C-h>"] = { "show_documentation", "hide_documentation" },
+			["<C-e>"] = { "hide", "fallback" },
+			["<CR>"] = { "accept", "fallback" },
 
-      ["<Tab>"] = { "snippet_forward", "select_next", "fallback" },
-      ["<S-Tab>"] = { "snippet_backward", "fallback" },
+			["<Tab>"] = { "snippet_forward", "select_next", "fallback" },
+			["<S-Tab>"] = { "snippet_backward", "fallback" },
 
-      ["<Up>"] = { "select_prev", "fallback" },
-      ["<Down>"] = { "select_next", "fallback" },
-      ["<C-p>"] = { "select_prev", "fallback" },
-      ["<C-n>"] = { "select_next", "fallback" },
+			["<Up>"] = { "select_prev", "fallback" },
+			["<Down>"] = { "select_next", "fallback" },
+			["<C-p>"] = { "select_prev", "fallback" },
+			["<C-n>"] = { "select_next", "fallback" },
 
-      ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-    },
+			["<C-b>"] = { "scroll_documentation_up", "fallback" },
+			["<C-f>"] = { "scroll_documentation_down", "fallback" },
+		},
 
-    completion = {
-      keyword = {
-        range = "prefix",
-      },
-      list = {
-        selection = function(ctx)
-          return ctx.mode == "cmdline" and "auto_insert" or "preselect"
-        end,
-      },
-      menu = {
-        border = "single",
-        draw = {
-          columns = {
-            { "label",     "label_description", gap = 1 },
-            { "kind_icon", "kind",              "source_name", gap = 1 },
-          },
-        },
-      },
-      documentation = { window = { border = "single" } },
-      trigger = {
-        show_on_trigger_character = true,
-      },
-    },
+		completion = {
+			keyword = {
+				range = "prefix",
+			},
+			list = {
+				selection = {
+					auto_insert = true,
+					preselect = function(ctx)
+						return ctx.mode ~= "cmdline"
+					end,
+				},
+			},
+			menu = {
+				border = "single",
+				draw = {
+					columns = {
+						{ "label", "label_description", gap = 1 },
+						{ "kind_icon", "kind", "source_name", gap = 1 },
+					},
+				},
+			},
+			documentation = { window = { border = "single" } },
+			trigger = {
+				show_on_trigger_character = true,
+			},
+		},
 
-    sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
-    },
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
 
-    signature = { window = { border = "single" } },
-  },
+		signature = { window = { border = "single" } },
+	},
 
-  opts_extend = { "sources.default" },
+	opts_extend = { "sources.default" },
 }
